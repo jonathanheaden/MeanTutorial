28cf40e6c5ec2e61ff6710f0a0069920d1c38b5a
Jonathan Headen <jonathan.headen@outlook.com>
2016-07-28 17:26:23 +1000

    first draft of express site, basic routes working

---
+++ app_server/controllers/locations.js
@@ -0,0 +1,15 @@
+/* GET 'home' page */
+module.exports.homelist = function (req, res) {
+    res.render('index', {title: 'Home'});
+};
+
+/* GET 'Location Info' page */
+module.exports.locationInfo = function (req, res) {
+    res.render('index', {title: 'Location Info'});
+};
+
+/* GET 'Add review' page */
+module.exports.addReview = function (req, res) {
+    res.render('index', {title: 'Add Review'});
+};
+
