6079017d8167c020013592773b2cdfa40cbc4171
Jonathan Headen <jonathan.headen@outlook.com>
2016-08-01 17:48:49 +1000

    moved data from view to controller for location detail page... layout seems wrong :(

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -31,7 +31,51 @@ module.exports.homelist = function (req, res) {
 
 /* GET 'Location Info' page */
 module.exports.locationInfo = function (req, res) {
-    res.render('location-info', {title: 'Location Info'});
+    res.render('location-info', {
+        title: 'Starcups',
+        pageHeader: {
+            title: 'Starcups'
+        },
+        sidebar: {
+            context: 'is on Loc8tr because it has accessible wifi and space to sit down with your laptop and get some work done.',
+            callToAction: 'If you\'ve been and you like it - or if you don\'t - please leave a review to help other people just like you.' 
+        },
+        location : {
+            name: 'Starcups',
+            address: '125 High Street, Reading, RG6 1PS',
+            rating: 3,
+            facilities: ['Hot Drinks', 'Food', 'Premium Wifi'],
+            coords: {
+                lat: 51.455041,
+                lng: -0.9690884
+            },
+            openingTimes: [{
+                days: 'Monday - Friday',
+                opening: '7:00 AM',
+                closing: '7:00 PM',
+                closed: false
+             }, { 
+                days: 'Monday - Friday',
+                opening: '8:00 AM',
+                closing: '5:00 PM',
+                closed: false
+             }, {
+                 days: 'Sunday',
+                 closed: true
+            }],
+            reviews : [{
+                author: 'Jonathan Headen',
+                rating: 4,
+                timestamp: '1st August 2016',
+                reviewText: 'Nice and quiet'
+             }, {
+                author: 'Charlie Chaplin',
+                rating: 2,
+                timestamp: '14th July 2015',
+                reviewText: 'It was too noisy'
+            }]
+        }  
+    });
 };
 
 /* GET 'Add review' page */
