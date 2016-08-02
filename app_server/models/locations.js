7a6a502133ed794a24716d74a9d07b810d7dc77a
Jonathan Headen <jonathan.headen@outlook.com>
2016-08-03 06:40:12 +1000

    required the model in db.js and added the schema to the model

--- app_server/models/locations.js
+++ app_server/models/locations.js
@@ -1,8 +1,8 @@
 var mongoose = require( 'mongoose' );
 
 var locationSchema = new mongoose.Schema({ 
-    name: String,
+    name: { type: String, required: true},
     address: String,
-    rating: Number,
+    rating: { type: Number, "default": 0, min: 0, max: 5 },
     facilities: [String]
 });
