534b91504cf02b0c41507c81eab8e53005b65039
Jonathan Headen <jonathan.headen@outlook.com>
2016-08-03 17:53:16 +1000

    wirefram placeholders for the controllers

---
+++ app_api/controllers/locations.js
@@ -0,0 +1,5 @@
+module.exports.locationsByDistance = function (req, res) { };
+module.exports.locationsCreate = function (req, res) { };
+module.exports.locationsReadOne = function (req, res) { };
+module.exports.locationsUpdateOne = function (req, res) { };
+module.exports.locationsDeleteOne = function (req, res) { };
