6c71014ad4864bed1fc391e70b0ebc49f4a7bac0
Jonathan Headen <jonathan.headen@outlook.com>
2016-08-03 17:56:55 +1000

    placeholder return codes

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -1,5 +1,20 @@
-module.exports.locationsByDistance = function (req, res) { };
-module.exports.locationsCreate = function (req, res) { };
-module.exports.locationsReadOne = function (req, res) { };
-module.exports.locationsUpdateOne = function (req, res) { };
-module.exports.locationsDeleteOne = function (req, res) { };
+module.exports.locationsByDistance = function (req, res) {
+    res.status(200);
+    res.json({"status" : "success"});
+ };
+module.exports.locationsCreate = function (req, res) { 
+    res.status(200);
+    res.json({"status" : "success"});
+};
+module.exports.locationsReadOne = function (req, res) {
+    res.status(200);
+    res.json({"status" : "success"});
+ };
+module.exports.locationsUpdateOne = function (req, res) { 
+    res.status(200);
+    res.json({"status" : "success"});
+};
+module.exports.locationsDeleteOne = function (req, res) { 
+    res.status(200);
+    res.json({"status" : "success"});
+};
--- app_api/controllers/reviews.js
+++ app_api/controllers/reviews.js
@@ -1,4 +1,16 @@
-module.exports.reviewsCreate = function (req, res) { };
-module.exports.reviewsReadOne = function (req, res) { };
-module.exports.reviewsUpdateOne = function (req, res) { };
-module.exports.reviewsDeleteOne = function (req, res) { };
+module.exports.reviewsCreate = function (req, res) { 
+    res.status(200);
+    res.json({"status" : "success"});
+};
+module.exports.reviewsReadOne = function (req, res) { 
+    res.status(200);
+    res.json({"status" : "success"});
+};
+module.exports.reviewsUpdateOne = function (req, res) { 
+    res.status(200);
+    res.json({"status" : "success"});
+};
+module.exports.reviewsDeleteOne = function (req, res) { 
+    res.status(200);
+    res.json({"status" : "success"});
+};
