12830dc5c973153ec1448366c931566b89718bb3
Jonathan Headen <jonathan.headen@outlook.com>
2016-08-10 07:10:25 +1000

    formatted distance and add units

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -24,10 +24,27 @@ module.exports.homelist = function (req, res) {
     request(
         requestOptions,
         function(err,response,body){
+            var i, data;
+            data = body;
+            for (i = 0; i < data.length; i++) {
+                data[i].distance = _formatDistance(data[i].distance);
+            }
             renderHomepage(req, res, body);
         }
     )
     
+};
+
+var _formatDistance = function (distance){
+    var numDistance, unit;
+    if (distance > 1) {
+        numDistance = parseFloat(distance).toFixed(1);
+        unit = 'km'
+    } else {
+        numDistance = parseInt(distance * 100,10);
+        unit = 'm'
+    }
+    return numDistance + unit;
 };
 
 var renderHomepage = function (req, res, responseBody) {
