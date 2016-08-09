cf1c11285422f6f07babf9e77f8ff2da656530ab
Jonathan Headen <jonathan.headen@outlook.com>
2016-08-09 17:12:51 +1000

    add apiOptions to read env and rewrite urls are appropriate

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,5 +1,12 @@
 var request = require('request');
 
+var apiOptions = {
+    server: "http://localhost:3000"
+};
+if (process.env.NODE_ENV === 'production') {
+    apiOptions.server = "https://herokuprodpath.herokuapp.com";
+}
+
 /* GET 'home' page */
 module.exports.homelist = function (req, res) {
     res.render('locations-list', {
