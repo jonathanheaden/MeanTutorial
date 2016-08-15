caa9ac243ab7d35ad1048775354103b39661a1e9
Jonathan Headen <jonathan.headen@outlook.com>
2016-08-16 07:09:44 +1000

    added data connection via service for home page

--- public/angular/loc8trApp.js
+++ public/angular/loc8trApp.js
@@ -31,11 +31,17 @@ var ratingStars = function(){
     };
 };
 
-var locationListCtrl = function($scope){
+var locationListCtrl = function($scope, loc8trData){
     $scope.data = {
-        locations: [{              
+        locations: loc8trData
+    };
+};
+
+var loc8trData = function() {
+    return [{
+                   
             "distance": 0.120,
-            "name": "Sargon",
+            "name": "Bean Blue",
             "address": "Collins Street, Melbourne",
             "rating": 3,
             "facilities": [
@@ -44,25 +50,12 @@ var locationListCtrl = function($scope){
             " wifi"
             ],
             "_id": "57a97e4c95355ad3537ca56e"
-        },
-        {
-            "distance": 0.0033799187040212883,
-            "name": "Great Space",
-            "address": "Collins Street, Melbourne",
-            "rating": 4,
-            "facilities": [
-            "good coffee",
-            " power",
-            " wifi, cakes"
-            ],
-            "_id": "57aa441c5ec03d635c4339a9"
-        }]
-    };
-};
-
+    }];
+}
 angular 
     .module('loc8trApp')
     .controller('locationListCtrl', locationListCtrl)
     .filter('formatDistance', formatDistance)
-    .directive('ratingStars', ratingStars);
+    .directive('ratingStars', ratingStars)
+    .service('loc8trData',loc8trData);
 
