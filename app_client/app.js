36ffea9fdcdbab08d1726500e96062141ef9ffab
Jonathan Headen <jonathan.headen@outlook.com>
2016-08-17 17:19:42 +1000

    wireframe of angular app

--- app_client/app.js
+++ app_client/app.js
@@ -1,1 +1,12 @@
-angular.module('loc8trApp', []);
+angular.module('loc8trApp', ['ngRoute']);
+
+function config($routeProvider){
+    $routeProvider
+       .when('/', {
+       })
+       .otherwise({redirectTo: '/'});
+}
+
+angular
+   .module('loc8trApp')
+   .config(['$routeProvider', config]);
