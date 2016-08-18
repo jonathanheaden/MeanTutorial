00e959002db3a8f9d29a095bb023ff893b08b0d4
Jonathan Headen <jonathan.headen@outlook.com>
2016-08-18 17:43:12 +1000

    typo and route change

--- app_client/home/home.controller.js
+++ app_client/home/home.controller.js
@@ -2,13 +2,58 @@ angular
   .module('loc8trApp')
   .controller('homeCtrl', homeCtrl);
 
-function homeCtrl($scope) {
+function homeCtrl($sope, loc8trData, geolocation) {
     var vm = this;
     vm.pageHeader = {
         title: 'Loc8tr',
-        strapline: 'Find places to wrok with wifi'
+        strapline: 'Find places to work with wifi'
     };
     vm.sidebar = {
         content: "Looking for wifi and a seat etc etc"
     };
+
+    vm.message = "Checking your location";
+    
+    vm.getData = function (position) {
+        var lat = position.coords.latitude,
+            lng = position.coords.longitude;
+        vm.message = "Searching for nearby places";
+        loc8trData.locationByCoords(lat,lng)
+        .success(function(data){
+            vm.message = data.length > 0 ? "" : "No nearby places found"
+            vm.data = { locations: data };
+        })
+            .error(function (e){
+                vm.message = "something went wrong";
+                console.log(e);
+            });
+    };
+
+    vm.showError = function(error){
+
+        $scope.$apply(function(){
+               vm.message = "using the hardcoded coordinates"
+                var lng = 3.969085,
+                    lat = -1.9690887;
+                loc8trData.locationByCoords(lat,lng)
+                .success(function(data){
+                    vm.message = data.length > 0 ? "" : "No nearby places found"
+                    vm.data = { locations: data };
+                })
+                    .error(function (e){
+                        vm.message = "something went wrong";
+                        console.log(e);
+                    });
+            });
+   
+    };
+
+    vm.noGeo = function(){
+        $scope.$apply(function(){
+            vm.message = "GeoLocation not available";
+        });
+    };
+
+    geolocation.getPosition(vm.getData, vm.showError, vm.noGeo);
+
 }
