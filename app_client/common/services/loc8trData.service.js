eaf4aab8f679d45d92ce4d68e75e6fd3c5d7d967
Jonathan Headen <jonathan.headen@outlook.com>
2016-08-18 17:43:00 +1000

    add geolocationand loc8trData services

---
+++ app_client/common/services/loc8trData.service.js
@@ -0,0 +1,12 @@
+angular
+  .module('loc8trApp')
+  .service('loc8trData', loc8trData);
+
+function loc8trData($http) {
+    var locationByCoords = function(lat, lng) {
+        return $http.get('/api/locations?lng='+lng+'&lat='+lat+'&maxdistance=20');
+    };
+    return {
+        locationByCoords : locationByCoords
+    };
+}
