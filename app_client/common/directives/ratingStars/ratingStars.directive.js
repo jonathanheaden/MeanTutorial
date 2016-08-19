c3abd1a59b2dda461ac2fbe66f7a71c8f3b3fb44
Jonathan Headen <jonathan.headen@outlook.com>
2016-08-19 17:04:32 +1000

    add ratingstars directive files

---
+++ app_client/common/directives/ratingStars/ratingStars.directive.js
@@ -0,0 +1,13 @@
+angular
+  .module('loc8trApp')
+  .directive('ratingStars', ratingStars);
+
+function ratingStars (){
+    return{
+        restrict: 'EA',
+        scope : {
+            thisRating: '=rating'
+        },
+        templateUrl : '/common/directives/ratingStars/ratingStars.template.html'
+    };
+}
