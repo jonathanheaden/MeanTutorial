f5409d4fcd461b587279599416f6bb76989ed3d7
Jonathan Headen <jonathan.headen@outlook.com>
2016-08-22 07:14:46 +1000

    adding the two new directives to minification array

--- app.js
+++ app.js
@@ -22,6 +22,8 @@ var appClientFiles = [
   'app_client/common/services/loc8trData.service.js',
   'app_client/common/services/geolocation.service.js',
   'app_client/common/directives/ratingStars/ratingStars.directive.js',
+  'app_client/common/directives/navigation/navigation.directive.js',
+  'app_client/common/directives/footerGeneric/footerGeneric.directive.js',
   'app_client/common/filters/formatDistance.filter.js'    
 ];
 var uglified = uglifyJs.minify(appClientFiles, {compress : false});
