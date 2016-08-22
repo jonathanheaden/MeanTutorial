0c4dfa8ba156da4fcac0cf2346b6f976a34327ed
Jonathan Headen <jonathan.headen@outlook.com>
2016-08-23 07:07:37 +1000

    fix up the routes in app.js for api to work again. Still not working  properly but the data is being retrieved from the api

--- app.js
+++ app.js
@@ -46,12 +46,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'app_client')));
 
 //app.use('/', routes);
-app.use(function(req,res) {
-  res.sendfile(path.join(___dirname, 'app_client', 'index.html'));
-});
 app.use('/api', routesApi);
 app.use('/users', users);
-
+app.use(function(req,res) {
+  res.sendfile(path.join(__dirname, 'app_client', 'index.html'));
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
