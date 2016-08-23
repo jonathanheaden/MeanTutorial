fb7dca3078b170f85365ed472955e8910ec2355d
Jonathan Headen <jonathan.headen@outlook.com>
2016-08-24 07:06:40 +1000

    add in angular sanitize

--- app_client/app.js
+++ app_client/app.js
@@ -1,5 +1,5 @@
 (function() {
-    angular.module('loc8trApp', ['ngRoute']);
+    angular.module('loc8trApp', ['ngRoute', 'ngSanitize']);
 
     function config($routeProvider, $locationProvider){
         $routeProvider
