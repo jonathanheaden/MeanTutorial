407a2126f3e10478c412201dad5e49ae2baeec83
Jonathan Headen <jonathan.headen@outlook.com>
2016-08-24 17:21:43 +1000

    add loc8trData service to location detail to start pulling data from the api

--- app_client/locationDetail/locationDetail.controller.js
+++ app_client/locationDetail/locationDetail.controller.js
@@ -4,13 +4,20 @@
       .module('loc8trApp')
       .controller('locationDetailCtrl', locationDetailCtrl);
 
-    locationDetailCtrl.$inject = ['$routeParams'];
-    function locationDetailCtrl($routeParams){
+    locationDetailCtrl.$inject = ['$routeParams', 'loc8trData'];
+    function locationDetailCtrl($routeParams, loc8trData){
         var vm = this;
 
         vm.locationid = $routeParams.locationid;
-        vm.pageHeader = {
-            title: vm.location
-        };
+        loc8trData.locationById(vm.locationid)
+          .success(function(data){
+              vm.data = { location : data };
+              vm.pageHeader = {
+                  title : vm.data.location.name
+              };
+          })
+          .error(function(e){
+              console.log(e);
+          });
     }
 })();
