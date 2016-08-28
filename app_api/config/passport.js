0ffeedd1f7ece9d0a0d5fb1a195227b9f56713d2
Jonathan Headen <jonathan.headen@outlook.com>
2016-08-28 17:09:23 +1000

    typo in passport.js

--- app_api/config/passport.js
+++ app_api/config/passport.js
@@ -3,7 +3,7 @@ var localStrategy = require('passport-local').Strategy;
 var mongoose = require('mongoose');
 var User = mongoose.model('User');
 
-passport.user(new localStrategy({
+passport.use(new localStrategy({
         usernameField: 'email'
     },
     function(username, password, done) {
