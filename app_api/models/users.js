a52fdb32f88e6a2dded2a3481a6f5c1a6c8cd802
Jonathan Headen <jonathan.headen@outlook.com>
2016-08-29 06:45:46 +1000

    addpassword validation methods to user model

--- app_api/models/users.js
+++ app_api/models/users.js
@@ -17,12 +17,12 @@ var userSchema = new mongoose.Schema({
 });
 
 userSchema.methods.setPassword = function(password) {
-    this.salt = crypto.randomBytes(16).tostring('hex');
-    this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).tostring('hex');
+    this.salt = crypto.randomBytes(16).toString('hex');
+    this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
 };
 
 userSchema.methods.validPassword = function(password) {
-    var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).tostring('hex');
+    var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
     return this.hash === hash
 };
 
