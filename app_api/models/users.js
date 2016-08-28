789f94538043322858c7831c637238728561d496
Jonathan Headen <jonathan.headen@outlook.com>
2016-08-28 11:25:03 +1000

    Adding the user schema to DB

---
+++ app_api/models/users.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose');
+var crypto= require('crypto');
+
+var userSchema = new mongoose.Schema({
+    email: {
+        type : String,
+        unique : true,
+        required : true
+    },
+    name : {
+        type : String,
+        required : true
+    }, 
+    hash : String,
+    salt: String
+});
+
+userSchema.methods.setPassword = function(password) {
+    this.salt = crypto.randomBytes(16).tostring('hex');
+    this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).tostring('hex');
+};
