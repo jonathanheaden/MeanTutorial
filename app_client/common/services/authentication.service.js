fd4ee54940bdb77cc16240e3cfc6fe5045ed4bb6
Jonathan Headen <jonathan.headen@outlook.com>
2016-08-29 17:31:49 +1000

    add login/out register to authentication service

--- app_client/common/services/authentication.service.js
+++ app_client/common/services/authentication.service.js
@@ -3,17 +3,34 @@
       .module('loc8trApp')
       .service('authentication', authentication);
     
-    authentication.$inject = ['$window'];
-    function authentication ($window){
+    authentication.$inject = ['$window', '$http'];
+    function authentication ($window, $http){
         var saveToken = function(token){
             $window.localStorage['loc8tr-token'] = token;
         };
         var getToken = function () {
             return $window.localStorage['loc8tr-token'];
         };
+        register = function(user) {
+            return $http.post('/api/register',user).success(function(data){
+                saveToken(data.token);
+            });
+        };
+        login = function(user) {
+            return $http.post('/api/login', user.success(function(data){
+                saveToken(data.token);
+            }));
+        };
+        logout = function() {
+            $window.localStorage.removeItem('loc8tr-token');
+        };
+
         return {
             saveToken : saveToken,
-            getToken : getToken
+            getToken : getToken,
+            register : register,
+            login : login,
+            logout : logout
         };
     }
 })();
