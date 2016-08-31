0935c21c45aabaaf879bcdd25b205719fbbc9371
Jonathan Headen <jonathan.headen@outlook.com>
2016-08-31 17:40:43 +1000

    login working but manual editing of minified file was required: the login function calling the API had brackets in the wrong spot

--- app_client/auth/login/login.controller.js
+++ app_client/auth/login/login.controller.js
@@ -6,7 +6,6 @@
     loginCtrl.$inject = ['$location','authentication'];
     function loginCtrl($location,authentication) {
         var vm = this;
-
         vm.pageHeader = {
             title : 'Login with your Loc8tr account'
         };
@@ -17,16 +16,17 @@
         vm.returnPage = $location.search().page || '/';
 
         vm.onSubmit = function() {
+            console.log('hello ' + vm.credentials.email);
             vm.formError = "";
             if (!vm.credentials.email || !vm.credentials.password) {
                 vm.formError = "All fields requires, please try again";
                 return false;
             } else {
-                vm.dologin();
+                vm.doLogin();
             }
         };
 
-        vm.dologin = function(){
+        vm.doLogin = function(){
             vm.formError = "";
             authentication
               .login(vm.credentials)
@@ -40,3 +40,8 @@
         };
     }
 })();
+
+
+
+//register=function(e){return t.post("/api/register",e).success(function(e){o(e.token)})};
+//login=function(e){return t.post("/api/login",e.success(function(e){o(e.token)}))};
