cd6b21c96b227a97035b4a6f88fff8264c900ad0
Jonathan Headen <jonathan.headen@outlook.com>
2016-09-01 07:22:11 +1000

    create the navigation controller (binding not working)

--- app_client/common/directives/navigation/navigation.controller.js
+++ app_client/common/directives/navigation/navigation.controller.js
@@ -7,5 +7,14 @@
     function navigationCtrl($location,authentication){
         var vm = this;
         vm.currentPath = $location.path();
-        }
+           
+        vm.isLoggedIn = authentication.isLoggedIn();
+
+        vm.currentUser = authentication.currentUser();
+
+        vm.logout = function(){
+            authentication.logout();
+            $location.path ('/');
+        };
+    }
 })();
